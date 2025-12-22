2a40b8c5fb2f5e1aad6a7aa04d2d6b6a562df5ba
SnehalKamble <sk3149478@gmail.com>
2025-12-22 20:22:58 +0530

    20 Dec Assignment

--- Assignment/Day5/Que4/main.py
+++ Assignment/Day5/Que4/main.py
@@ -1,12 +1,8 @@
-# Import datetime module
-import datetime
 
-# Get current date and time
+import datetime
 current_datetime = datetime.datetime.now()
 
-# Display current date and time
 print("Current Date and Time:", current_datetime)
 
-# Print day of the week
 day_of_week = current_datetime.strftime("%A")
 print("Day of the Week:", day_of_week)
